235cd5af417cc73212a3004f0a0b67615aabfb83
Deisel <74831846+Amelia-was@users.noreply.github.com>
2020-12-11 16:56:13 -0500

    portfolio basic html and css

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// code from CSS Tricks //
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
